f9ce68db9d8a6033ac593296ad01e3a73e46ca00
Daniel Yang <shu.minyang@hotmail.com>
2022-10-19 22:12:20 -0500

    first commit

---
+++ src/components/MessageInput.tsx
@@ -0,0 +1,5 @@
+export default function MessageInput() {
+  return (
+    <textarea class="textarea-bordered textarea mx-2 mt-auto mb-4 focus:border-0" />
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import MessageInput from "~/components/MessageInput";
+
+export default function Home() {
+  return (
+    <main class="flex h-full">
+      <div class="m-auto flex h-[calc(100%-12px)] w-[calc(100%-12px)] flex-col rounded-md border">
+        aiosdjioass
+        <MessageInput />
+      </div>
+    </main>
+  );
+}
